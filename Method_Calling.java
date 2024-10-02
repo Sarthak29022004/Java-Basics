5843f9a2363eb77b9e3cfd0e5abbce8700e2bd48
Sarthak29022004 <sarthakbobhate@gmail.com>
2024-10-02 11:09:18 +0530

    add Method_Calling

---
+++ Method_Calling.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Method_Calling {
+    public static void main(String []args){
+        int a = 10;
+        int b = 15;
+        System.out.println("Sarthak");
+        abc cl = new abc();
+        cl.add();
+        Sub();
+    }
+    public void add() { //We need to create object for public method and then call it
+        Scanner sc = new Scanner(System.in);
+        int C = sc.nextInt();
+        int D = sc.nextInt();
+        System.out.println(C + D);
+    }
+    static void Sub() {  //We don't need to create object, we can directly call static method 
+        Scanner sc = new Scanner(System.in);
+        int C = sc.nextInt();
+        int D = sc.nextInt();
+        System.out.println(C + D);
+    }
+}
